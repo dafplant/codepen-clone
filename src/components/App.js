3dc21eec02a70598f666cb20d3232a2f976952ec
Chris Plant <chris.daf@gmail.com>
2020-09-23 14:52:36 +0100

    added Editor component, styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../index.css";
+import Editor from "./Editor";
 
 function App() {
-  return "Hello World";
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="Javascript"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane"></div>
+      <iframe
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      />
+    </>
+  );
 }
 
 export default App;
